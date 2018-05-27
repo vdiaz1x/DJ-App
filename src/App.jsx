750da05df34f0039dd5d5bdd2e030f2155e99d79
Vinicio Diaz <vdiaz1x@gmail.com>
2018-05-27 18:51:40 -0400

    normalized volume to crossfader, added comments to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,9 +16,6 @@ class App extends Component {
     super(props);
     this.state = {
       SM: {},
-      play: false,
-      value: 10,
-      // stop: false,
       vol: {
         v1: 50,
         v2: 50,
@@ -37,7 +34,9 @@ class App extends Component {
   // component will mount initialization
   componentWillMount() {
     this.setState({
+      // initializing the soundmanager
       SM: SM.setup({
+        // creating sound objects here
         onready() {
           SM.createSound({
             id: 'test',
@@ -47,6 +46,10 @@ class App extends Component {
             id: 'synth',
             url: 'https://freesound.org/data/previews/429/429953_2731495-lq.ogg',
           });
+        },
+        // loading the song files as soon as they are created for faster playback later
+        defaultOptions: {
+          autoLoad: true,
         },
       }),
     });
@@ -58,39 +61,65 @@ class App extends Component {
   //   SM.play('test');
   // }
 
+  // plays song/pauses song (depending on if song is alredy playing
+  // takes song id as parameter
   play(song) {
     // console.log(this.state.play);
-    !this.state.play ? SM.play(song) : SM.pause(song);
-    this.setState({ play: !this.state.play });
+    SM.togglePause(song);
   }
 
+  // stops song, reset playtime to beginning
   stop(song) {
     SM.stop(song);
   }
 
+  // sets the volume of the song playing
+  // takes volume number (value of slider), song id, and right/left side as parameters
   volume(vol, song, side) {
+    // setting both the volume values for later use
     this.setState({
       vol: {
+        // set to ensure both volumes exist
         v1: this.state.vol.v1,
         v2: this.state.vol.v2,
+        // setting the value of the song that is actually being changed
         [side]: vol,
       },
     });
+
+    // setting the volume of the song by the id
     SM.setVolume(song, vol);
   }
 
+  // this crossfades the two songs (which song is playing at any given time)
+  // takes the crossfade number (value of slider) as parameter
+  // the slider value goes from -100 to 100
+  // when cf value is zero, both songs play. at -100, only the left song plays, when 100, only the right song plays
   crossfade(cfade) {
+    // setting the crossfade value for later use
     this.setState({
       cfade,
-    // SM.setVolume()
     });
+
+    // normalizing the crossfade
+    // when cf value is below zero, cf2 decrements, cf1 stays at 100
+    // when cf value is above zero, cf1 decrements, cf2 stays at 100
     const cf1 = this.state.cfade > 0 ? 100 - cfade : 100;
     const cf2 = this.state.cfade < 0 ? 100 + cfade : 100;
-    console.log(cf1);
-    console.log(cf2);
+
+    // normalizing the volume so the cf value doesn't reset the volume level
+    // sets the ratio of the cf values above to the volume levels for the songs as new volume levels for those songs
+    const v1 = (cf1 * this.state.vol.v1) / 100;
+    const v2 = (cf2 * this.state.vol.v2) / 100;
+
+    // console.log(v2);
+    // console.log(cf1);
+    // console.log(cf2);
     // console.log(this.state.cfade);
-    SM.setVolume('test', cf1);
-    SM.setVolume('synth', cf2);
+
+    // sets the volume of the songs for crossfading
+    SM.setVolume('test', v1);
+    SM.setVolume('synth', v2);
   }
 
   // render
